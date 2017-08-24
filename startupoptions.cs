b4d8664bcfc1077d36b6fd2505698805cee2fde5
juriman <ultrajuri@hotmail.com>
2017-08-24 06:00:17 +0200

    add idea for stratup parameter data class

--- startupoptions.cs
+++ startupoptions.cs
@@ -1,3 +1,17 @@
 public class StartupOptions{
+  public bool LoggingEnabled;
+  // todo: this shall replace SettngsLocation.xml
+  string LoggingFolder;
+
+  bool ConfifEnabled;
+  string ConfigFolder;
+
+  bool ProcedureDataCollectionEnabled;
+  string DataFolder;
+
+  bool SqlDatabaseEnabled;
+  
+  // Default is execution root folder
+  string CustomModleAssembliesFolder;
 
 }
